f798ae9b7c41c3d0e5fdf76691e54182de1522be
Lauren Burnett <Lauren@jf.local>
2016-06-09 16:11:16 -0700

    Add styling to results and define input variables in script

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $("#userData").submit(function(event)  {
     event.preventDefault();
-    var fieldInput = $("input#userName").val();
+    var userNameInput = $("input#userName").val();
+    var weatherInput = $("#weather").val();
+    var activityInput = $("input:radio[name=activity]:checked").val();
+    var priorityInput = $("#priority").val();
+    var personalityInput = ("input:radio[name=personality]:checked").val(;)
 
     $(".userName").text(fieldInput);
 
