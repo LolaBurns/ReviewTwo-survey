d17caa8a28a0c32039000abfc811d2df4f3c2c7e
LEB <lauren.e.burnett@gmail.com>
2016-06-10 12:49:23 -0700

    Add results scripts and styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,16 @@ $(document).ready(function() {
     var weatherInput = $("#weather").val();
     var activityInput = $("input:radio[name=activity]:checked").val();
     var priorityInput = $("#priority").val();
-    var personalityInput = ("input:radio[name=personality]:checked").val(;)
+    var personalityInput = $("input:radio[name=personality]:checked").val();
+    var results = weatherInput + activityInput + priorityInput + personalityInput;
 
-    $(".userName").text(fieldInput);
+    if ((results === "aaaa") || (results === "abaa") || (results === "aaba") || (results === "aaab") || (results === "baaa") || (results === "caaa") || (results === "acaa") || (results === "aaca") || (results === "aaac")) {
+      results = ("The Bahamas");
+    } else if ((results === "cccc") || (results === "cacc") || (results === "ccac") || (results === "ccca") || (results === "cbcc") || (results === "ccbc") || (results === "cccb") ) { results = ("Alaska");
+    }
+
+    $(".userName").text(userNameInput);
+    $(".resultSpot").text(results);
 
     $("#results").show();
     $("#userData").hide();
